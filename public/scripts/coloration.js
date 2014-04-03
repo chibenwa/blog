e6ddf02f3ac4dac67fd17de28134c62c1ee4b1b7
benwa <benwa@minet.net>
2014-04-03 02:31:17 +0200

    Adding syntax highlighting

---
+++ public/scripts/coloration.js
@@ -0,0 +1,10 @@
+var pres = document.getElementsByTagName("pre");
+     for( var i = 0; i < pres.length; i++ ) {
+	 var my_code = pres[i].firstChild;
+	 if( my_code != undefined ) {
+	     var text = my_code.innerHTML;
+	     pres[i].innerHTML = text;
+	     pres[i].className = "brush: js";
+	 } 
+     }
+     SyntaxHighlighter.all();
